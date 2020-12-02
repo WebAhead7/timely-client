e104fb0676b2998d3510bff87c3f57ec19a9a85f
Muhammad Awwad <hammode.awad@gmail.com>
2020-12-02 15:07:24 +0200

    deleted repeated calls

--- src/App/App.js
+++ src/App/App.js
@@ -1,39 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Home from "../screens/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "../screens/Login";
 import Profile from "../screens/Profile";
 import "./App.css";
 
-import Clinic from "../screens/Clinic";
-import { getDocList } from "../Api/api";
-import "./App.css";
-import React, { useEffect } from "react";
-import Home from "../screens/Home";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Navbar from "../components/Navbar";
-
-
 function App() {
+  const clientObj = {
+    firstname: "Muhammad",
+    lastname: "Awwad",
+    email: "hammode.awad@gmail.com",
+    imgUrl:
+      "https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png",
+  };
 
-  const [list, setList] = useState(null);
+  const doctorObj = {
+    firstname: "John",
+    lastname: "Steve",
+    email: "john_steve@hotmail.com",
+    imgUrl:
+      "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_535,h_516/https://bestpriceweb.net/wp-content/uploads/2020/01/485165_PR_130_SI_24_06_14_255-m.png",
+    title: "Eyes",
+    dsc:
+      "John Steve, Eyes Doctor,    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tempora consequatur dolor enim placeat necessitatibus hic nihil illo quod aliquid doloremque perferendis, sequi asperiores facere voluptate. Repellendus consectetur porro perspiciatis.",
+  };
 
-  useEffect(() => {
-    getDocList(setList);
-  }, []);
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar /> 
+        {/* <Navbar /> */}
         <Switch>
           <Route exact path="/">
-            <Home list={list} />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/clinic">
-            <Clinic />
+            <Home />
           </Route>
           <Route path="/login">
             {" "}
@@ -47,24 +45,5 @@ function App() {
     </div>
   );
 }
-
-const clientObj = {
-  firstname: "Muhammad",
-  lastname: "Awwad",
-  email: "hammode.awad@gmail.com",
-  imgUrl:
-    "https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png",
-};
-
-const doctorObj = {
-  firstname: "John",
-  lastname: "Steve",
-  email: "john_steve@hotmail.com",
-  imgUrl:
-    "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_535,h_516/https://bestpriceweb.net/wp-content/uploads/2020/01/485165_PR_130_SI_24_06_14_255-m.png",
-  title: "Eyes",
-  dsc:
-    "John Steve, Eyes Doctor,    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tempora consequatur dolor enim placeat necessitatibus hic nihil illo quod aliquid doloremque perferendis, sequi asperiores facere voluptate. Repellendus consectetur porro perspiciatis.",
-};
 
 export default App;
