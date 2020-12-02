76c29b34ea73440e9848a7c5a264fb904adae40a
OmklthomAmara <amaraomklthom@gmail.com>
2020-12-02 19:46:28 +0200

    signup component

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,29 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Home from "../screens/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "../screens/Login";
+import Signup from "../screens/Signup";
+
 import "./App.css";
 
 function App() {
+  
+  const [sucsess, setSucsess] = React.useState(false);
+
   return (
     <div className="App">
       <BrowserRouter>
         {/* <Navbar /> */}
         <Switch>
           <Route exact path="/">
-            <Home />
+          { sucsess? <Home /> :  <Login  setSucsess={setSucsess}/>}  
           </Route>
-          <Route path="/login" component={Login} />
+          <Route path="/signup"> 
+          <Signup/>
+          </Route>
+          <Route path="/login" > 
+          <Login setSucsess={setSucsess}/>
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
--- src/screens/Signup/index.jsx
+++ src/screens/Signup/index.jsx
@@ -1,11 +1,145 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-export default function Signup() {
-    return (
-        <div>
-            
+export default function Signup(props) {
+  const [checked, setChecked] = React.useState(false);
+  const [firstname, setFirstName] = React.useState("");
+  const [lastname, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [pass, setPass] = React.useState("");
+  const [imgUrl, setImgUrl] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [dsc, setDsc] = React.useState("");
+  const [msg, setMsg] = React.useState("");
 
-            
-        </div>
-    )
+  const history = useHistory();
+
+  function redirect() {
+    history.push("/");
+  }
+
+  function handleSubmit() {
+    const data = { firstname, lastname, email, pass, imgUrl, title, dsc };
+    const url = `http://localhost:4000/${checked ? "doctor" : "client"}/signup`;
+    console.log("url", url);
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((msg) => {
+        setMsg(msg.msg);
+        props.setSucsess((sucsess) => (sucsess = msg.auth));
+
+        if (msg.auth) {
+          redirect();
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="fname">First name:</label>
+        <br />
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          required
+          value={firstname}
+          onChange={(event) => setFirstName(event.target.value)}
+        />
+        <br />
+        <label htmlFor="lname">Last name:</label>
+        <br />
+        <input
+          type="text"
+          id="lname"
+          name="lname"
+          required
+          value={lastname}
+          onChange={(event) => setLastName(event.target.value)}
+        />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <br />
+        <label htmlFor="pass">Password:</label>
+        <br />
+        <input
+          type="text"
+          id="pass"
+          name="pass"
+          required
+          value={pass}
+          onChange={(event) => setPass(event.target.value)}
+        />
+        <br />
+        <label htmlFor="imgUrl">Image:</label>
+        <br />
+        <input
+          type="text"
+          id="imgUrl"
+          name="imgUrl"
+          required
+          value={imgUrl}
+          onChange={(event) => setImgUrl(event.target.value)}
+        />
+        <br />
+        <br />
+        <label htmlFor="doctor"> I'm a Doctor?</label>
+        <input
+          type="checkbox"
+          id="doctor"
+          name="doctor"
+          value="doctor"
+          required
+          checked={checked}
+          onChange={(event) => setChecked(event.target.checked)}
+        />
+      </form>
+      <br />
+      {checked && (
+        <form>
+          <label htmlFor="title">Specialization:</label>
+          <br />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <br />
+          <label htmlFor="dsc">Description:</label>
+          <br />
+          <input
+            type="text"
+            id="dsc"
+            name="dsc"
+            required
+            value={dsc}
+            onChange={(event) => setDsc(event.target.value)}
+          />
+          <br />
+        </form>
+      )}
+
+      <button onClick={handleSubmit}>SUBMIT</button>
+
+      <h1>{msg}</h1>
+    </div>
+  );
 }
