3401e7f64c7fb098e1b5e2ef0ff1963fdd8011b6
myassar1211 <myassar1@gmail.com>
2020-12-02 14:53:47 +0200

    update

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import React, { useEffect } from "react";
+import Home from "../screens/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 
 function App() {
-  return <div className="App">App Component</div>;
-}
+  return (
+  <div className="App">
+<BrowserRouter>
+<Navbar /> 
+<Switch>
+<Route exact path="/">
+<Home />
+</Route>
+
+
+</Switch>
+</BrowserRouter>
+</div>
+
+  )}
+
 
 export default App;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./Navbar.css"
 
 export default function Navbar() {
     return (
-        <div>
-            
-        </div>
+       
+ 
+              <nav className="navbar">
+    <div className="container">
+    <div className="navbar-header">
+    </div>
+    <div className="collapse navbar-collapse" id="myNavbar">
+      <ul className="nav navbar-nav navbar-right">
+        <li id="home"><a href="/">HOME</a></li>
+        <li id="about"><a href="btn btn-success">ABOUT US</a></li>
+        <li id="contact us"><a href="btn btn-success">CONTACT US</a></li>
+        <li id="sign in"><a href="btn btn-primary">SIGN IN</a></li>
+        <li id="sign up"><a href="btn btn-primary">SIGN UP</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+      
     )
 }
