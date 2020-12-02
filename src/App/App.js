15f07e6e9064dc72908a85eaf57ffb124128c96e
Muhammad Awwad <hammode.awad@gmail.com>
2020-12-02 14:42:37 +0200

    worked on profile component

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,29 @@ import React, { useEffect } from "react";
 import Home from "../screens/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Login from "../screens/Login";
+import Profile from "../screens/Profile";
 import "./App.css";
 
 function App() {
+  const clientObj = {
+    firstname: "Muhammad",
+    lastname: "Awwad",
+    email: "hammode.awad@gmail.com",
+    imgUrl:
+      "https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png",
+  };
+
+  const doctorObj = {
+    firstname: "John",
+    lastname: "Steve",
+    email: "john_steve@hotmail.com",
+    imgUrl:
+      "https://cdn.shortpixel.ai/client/q_glossy,ret_img,w_535,h_516/https://bestpriceweb.net/wp-content/uploads/2020/01/485165_PR_130_SI_24_06_14_255-m.png",
+    title: "Eyes",
+    dsc:
+      "John Steve, Eyes Doctor,    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tempora consequatur dolor enim placeat necessitatibus hic nihil illo quod aliquid doloremque perferendis, sequi asperiores facere voluptate. Repellendus consectetur porro perspiciatis.",
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,7 +33,13 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/login" component={Login} />
+          <Route path="/login">
+            {" "}
+            <Login />{" "}
+          </Route>
+          <Route path="/profile">
+            <Profile dataObj={clientObj} />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
