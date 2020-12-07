69f59f84d19b1096cb2475c88dbabe35e424e01c
OmklthomAmara <amaraomklthom@gmail.com>
2020-12-07 18:23:49 +0200

    heroku to local

--- src/Api/api.js
+++ src/Api/api.js
@@ -2,7 +2,7 @@ const heroku = `https://timelyserver.herokuapp.com/`;
 const local = `http://localhost:4000/`;
 
 export const getDocList = async (setList) => {
-  const listUrl = `${heroku}main/get-list`;
+  const listUrl = `${local}main/get-list`;
   try {
     const data = await fetch(listUrl);
     const list = await data.json();
@@ -14,7 +14,7 @@ export const getDocList = async (setList) => {
 };
 
 export const getDocClinic = async (id, setClinic) => {
-  const clinicUrl = `${heroku}doctor/${id}/doctor-clinic`;
+  const clinicUrl = `${local}doctor/${id}/doctor-clinic`;
   try {
     const data = await fetch(clinicUrl);
     const clinic = await data.json();
@@ -26,7 +26,7 @@ export const getDocClinic = async (id, setClinic) => {
 };
 
 export const getProfile = async (id, setProfile) => {
-  const profileUrl = `${heroku}client/${id}/client-profile`;
+  const profileUrl = `${local}client/${id}/client-profile`;
   try {
     const data = await fetch(profileUrl);
     const profile = await data.json();
@@ -41,7 +41,7 @@ export const getProfile = async (id, setProfile) => {
 export const setAppointmentApi = async (conf) => {
   const { docid, clientid, day, hour } = conf;
 
-  const appointmentUrl = `${heroku}client/create-appointment/${docid}/${clientid}/${day}/${hour}`;
+  const appointmentUrl = `${local}client/create-appointment/${docid}/${clientid}/${day}/${hour}`;
   try {
     const response = await fetch(appointmentUrl);
     const json = await response.json();
